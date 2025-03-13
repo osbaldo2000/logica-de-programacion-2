0bd99822ee2c4628d24aca8f54eab4c524297fdf
Osbaldo <osbaldo.are2000@gmail.com>
2025-03-13 14:55:03 -0600

    ejercicio

---
+++ ejercicio.js
@@ -0,0 +1,17 @@
+let celsiusIngresados = [];
+
+let celsius = parseFloat(prompt("Ingresa un numero en grados Celsius: "));
+celsiusIngresados.push(celsius);
+
+function celsiusAFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function celsiusAKelvin(celsius) {
+    return celsius + 273.15;
+}
+
+let fahrenheit = celsiusAFahrenheit(celsius);
+let kelvin = celsiusAKelvin(celsius);
+
+console.log(`los gracis Celsius ingresados ${celsius} °C, Convertidos a Fahrenheit ${fahrenheit}°F y Kelvin ${kelvin}K`);
